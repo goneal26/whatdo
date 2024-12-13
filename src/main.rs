c8ba990b24ec0fca35d76a3c302d5021f6763770
aiko <g3oneal@icloud.com>
2024-12-13 04:55:21 -0500

    reworked to remove check and uncheck commands

--- src/main.rs
+++ src/main.rs
@@ -35,43 +35,17 @@ enum Commands {
   },
 
   /// Get list of all tasks
-  List {
-    /// Filter for checked tasks
-    #[clap(long, short, action)]
-    checked: bool,
-
-    /// Filter for unchecked tasks
-    #[clap(long, short, action)]
-    unchecked: bool,
-  },
-
-  /// Mark tasks as complete
-  #[command(arg_required_else_help(true))]
-  Check {
-    /// Space-separated list of tasks to mark as complete
-    tasks: Vec<String>,
-  },
-
-  /// Unmark tasks as complete
-  #[command(arg_required_else_help(true))]
-  Uncheck {
-    /// Space-separated list of tasks to unmark as complete
-    tasks: Vec<String>,
-  },
+  List,
 
   /// Remove checked tasks from the list
-  Clear {
-    /// Remove ALL tasks from the list (checked AND unchecked)
-    #[clap(long, action)]
-    all: bool,
-  },
+  Clear,
 
   /// Get the path of the global list file
-  Config, // TODO maybe change the names of these
+  Path,
 
   /// Change the path of the global list file (BROKEN)
   #[command(arg_required_else_help(true))]
-  SetConfig {
+  SetPath {
     /// Path to new global list file
     path: PathBuf,
   },
@@ -85,7 +59,7 @@ fn main() -> std::io::Result<()> {
 
   let mut path = env::current_dir()?;
   path.push("todo");
-  path.set_extension("md"); // TODO changing from default path?
+  path.set_extension("txt"); // TODO changing from default path?
 
   let mut list = List::new(path);
 
@@ -93,8 +67,8 @@ fn main() -> std::io::Result<()> {
     None | Some(Commands::Pick) => {
       let picked = list.pick();
       match picked {
-        Some(task) => println!("Picked: {}", task),
-        None => println!("No unfinished tasks to pick!"),
+        Some(task) => println!("{}", task),
+        None => println!("(No tasks to pick)"),
       };
     }
     Some(Commands::Add { tasks }) => {
@@ -107,30 +81,12 @@ fn main() -> std::io::Result<()> {
         list.remove(task);
       }
     }
-    Some(Commands::List { checked, unchecked }) => {
-      if !checked && !unchecked {
-        // i.e. no flags passed
-        println!("{}", list.get_list_as_string(true, true)); // display full list
-      } else {
-        // filter for only checked/unchecked items
-        println!("{}", list.get_list_as_string(checked, unchecked));
-      }
+    Some(Commands::List) => println!("{}", list.to_string()),
+    Some(Commands::Clear) => list.clear(),
+    Some(Commands::Path) => {
+      println!("{}", list.path().display());
     }
-    Some(Commands::Check { tasks }) => {
-      for task in tasks.iter() {
-        list.check(task);
-      }
-    }
-    Some(Commands::Uncheck { tasks }) => {
-      for task in tasks.iter() {
-        list.uncheck(task);
-      }
-    }
-    Some(Commands::Clear { all }) => list.clear(all),
-    Some(Commands::Config) => {
-      println!("{}", list.get_path().display());
-    }
-    Some(Commands::SetConfig { path }) => list.set_path(path),
+    Some(Commands::SetPath { path }) => list.set_path(path),
   };
 
   Ok(())
